17a46663cf66794c7e1e5f26680ebc7cf20cbcb6
simon-lc <simonlc@stanford.edu>
2021-02-02 17:32:48 -0800

    hard compat

--- src/Algames.jl
+++ src/Algames.jl
@@ -1,5 +1,9 @@
 module Algames
 
+Pkg.add(Pkg.PackageSpec(;name="RobotDynamics", version="0.3.1"))
+Pkg.add(Pkg.PackageSpec(;name="TrajectoryOptimization", version="0.4.1"))
+Pkg.add(Pkg.PackageSpec(;name="Altro", version="0.3.0"))
+
 greet() = print("Hello World!")
 
 using Altro
